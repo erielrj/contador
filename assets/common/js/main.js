97fc0f74337933b45c56d44a1609d021a520039e
erielrj <eriel.rj@gmail.com>
2021-07-23 09:19:51 -0300

    Steps adicionado

--- assets/common/js/main.js
+++ assets/common/js/main.js
@@ -1,5 +1,22 @@
+
+(function(steps) {
+    const select = document.querySelector("#steps");
+    
+    for(let step = 2; step <= steps; step++){
+        console.log(step);
+        option = document.createElement("option");
+        option.setAttribute('value', step);
+        optionValue = document.createTextNode(step);
+        option.appendChild(optionValue);
+        select.appendChild(option);
+        console.log(option);
+    }
+
+})(50);
+
 function increase(){
+    const select = document.querySelector("#steps");
     const visor = document.querySelector("#visor");
     
-    visor.textContent = Number(visor.textContent) + 1;
+    visor.textContent = Number(visor.textContent) + Number(select.value);
 }
